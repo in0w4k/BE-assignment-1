bad7b68d3abce807df99c73e3e2d0d5cb7751d41
in0w4k <in0w4.k@gmail.com>
2025-02-16 13:18:57 +0500

    Create first bean

---
+++ src/main/java/com/springproject/FirstBean.java
@@ -0,0 +1,10 @@
+package com.springproject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FirstBean {
+    public String sayHello() {
+        return "Hello Choomba!";
+    }
+}
