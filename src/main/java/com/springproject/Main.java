83a70258002b64f4477ee7600846d9903ff20c19
in0w4k <in0w4.k@gmail.com>
2025-02-16 13:20:00 +0500

    Create main java file

---
+++ src/main/java/com/springproject/Main.java
@@ -0,0 +1,14 @@
+package com.springproject;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        FirstBean firstBean = context.getBean(FirstBean.class);
+        SecondBean secondBean = context.getBean(SecondBean.class);
+        System.out.println(firstBean.sayHello());
+        System.out.println(secondBean.userName());
+    }
+}
