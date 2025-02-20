e55dbfbcc69de85c73657f46d78056c85989c780
in0w4k <in0w4.k@gmail.com>
2025-02-20 08:13:23 +0500

    change Main

--- src/main/java/com/springproject/Main.java
+++ src/main/java/com/springproject/Main.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.springproject");
         FirstBean firstBean = context.getBean(FirstBean.class);
         SecondBean secondBean = context.getBean(SecondBean.class);
         System.out.println(firstBean.sayHello());
